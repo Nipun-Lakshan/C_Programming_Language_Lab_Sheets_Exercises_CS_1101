259adb5e1a14422b39f0823a88f54d20d838a84b
Nipun Lakshan <nipunlakshan81@gmail.com>
2024-12-11 11:52:57 +0530

    Exercise 09

---
+++ Exercise09.c
@@ -0,0 +1,8 @@
+// Spacing, Escape Characters and Comments (Supplementary Worksheet Week 01 - Question 18)
+
+#include <stdio.h>
+int main()
+{
+    printf("\nMy File Path is : A:\\02. LMS - Temp\\CS 1101\\02. Coding Practice Files\\03. C Programming - Labsheets\n");
+    return 0;
+}
