c4a87434451f32450cd059d4c367d0076d0a6a3d
Nipun Lakshan <nipunlakshan81@gmail.com>
2024-12-11 10:59:00 +0530

    Exercise 04

---
+++ Exercise04.c
@@ -0,0 +1,11 @@
+// Spacing, Escape Characters and Comments (Supplementary Worksheet Week 01 - Question 14)
+
+#include <stdio.h>
+int main()
+{
+    printf("\nName    : A.W.W.A. Nipun Lakshan");
+    printf("\nAge     : 22");
+    printf("\nAddress : No. 554/C, Madatiyagahawatta Road, Gonahena, Kadawatha\n");
+
+    return 0;
+}
