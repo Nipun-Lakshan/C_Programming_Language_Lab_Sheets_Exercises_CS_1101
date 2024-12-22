c7c33f34e743ab4256126f8349ff702d2bb1153c
Nipun Lakshan <nipunlakshan81@gmail.com>
2024-12-22 19:32:05 +0530

    Exercise 11

---
+++ Exercise11.c
@@ -0,0 +1,52 @@
+// Flowcharts (Supplementary Worksheet Week 01 - Question 03)
+
+#include <stdio.h>
+
+int main()
+
+{
+
+    // Variable Declaration
+
+    float num1, num2, num3, max;
+
+    // Prompt User for Input
+
+    printf("\nEnter Number 01 : ");
+    scanf("%f", &num1);
+    printf("Enter Number 02 : ");
+    scanf("%f", &num2);
+    printf("Enter Number 03 : ");
+    scanf("%f", &num3);
+
+    // Calculate the Largest Number and Print the result
+
+    if (num1 == num2 && num2 == num3)
+    {
+        printf("\nThe inserted three numbers are equal!\n");
+    }
+
+    else
+    {
+
+        max = num1;
+
+        if (max <= num2)
+        {
+            max = num2;
+            if (max <= num3)
+            {
+                max = num3;
+            }
+        }
+
+        else if (max <= num3)
+        {
+            max = num3;
+        }
+
+        printf("\nThe largest number from %.2f, %.2f, and %.2f is %.2f.\n", num1, num2, num3, max);
+    }
+
+    return 0;
+}
