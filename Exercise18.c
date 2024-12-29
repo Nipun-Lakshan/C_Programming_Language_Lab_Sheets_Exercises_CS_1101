64d555cf81347558b66739ac595237a20fd7b09c
Nipun Lakshan <nipunlakshan81@gmail.com>
2024-12-29 11:11:38 +0530

    Exercise 18

---
+++ Exercise18.c
@@ -0,0 +1,19 @@
+// Flowcharts (Supplementary Worksheet Week 01 - Question 09)
+
+#include <stdio.h>
+
+int main()
+
+{
+    // Variable Declaration
+    int i; // Loop Index
+
+    // Print "Hello World" 10 Times
+    for (i = 0; i < 10; i++)
+    {
+        printf("\nHello World");
+    }
+    printf("\n");
+
+    return 0;
+}
---
+++ Exercise19.c
@@ -0,0 +1,16 @@
+// Flowcharts (Supplementary Worksheet Week 01 - Question 10)
+
+#include <stdio.h>
+
+int main()
+
+{
+    // Variable Declaration
+    long double area;
+    long double circumference;
+    long double radius;
+
+    // Start Here
+
+    return 0;
+}
