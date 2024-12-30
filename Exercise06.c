09ea45ff4b97e2b9e6aa63bd43248fe1536c076d
Nipun Lakshan <nipunlakshan81@gmail.com>
2024-12-30 15:45:25 +0530

    Exercise 24

--- Exercise06.c
+++ Exercise06.c
@@ -4,7 +4,8 @@
 int main()
 {
     printf("\nName    : A.W.W.A. Nipun Lakshan");
-    printf("\nDOB     : 2002/08/13");
+    printf("\nAge     : 22");
+    printf("\b\b\b\b\b\b\b\b\b\b\b\bDOB     : 2002/08/13");
     printf("\nAddress : No. 554/C, \n\t  Madatiyagahawatta Road, \n\t  Gonahena, \n\t  Kadawatha.\n");
 
     return 0;
--- Exercise24.c
+++ Exercise24.c
@@ -1,16 +1,9 @@
 // Variable in C (Supplementary Worksheet Week 01 - Question 19)
 
 #include <stdio.h>
-
 int main()
 {
-    // Variable Declaration
-    ;
-
-    // Header String Formatting
-    printf("\n=======================");
-    printf("\nFirst 100 Prime Numbers");
-    printf("\n=======================\n\n");
-
+    int one_1 = 25;
+    printf("%d", one_1);
     return 0;
 }
