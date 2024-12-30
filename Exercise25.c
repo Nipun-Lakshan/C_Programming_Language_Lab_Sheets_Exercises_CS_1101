1943090be51c1e515893587b666278da2d234762
Nipun Lakshan <nipunlakshan81@gmail.com>
2024-12-31 00:38:06 +0530

    Exercise 25

---
+++ Exercise25.c
@@ -0,0 +1,10 @@
+// Variable in C (Supplementary Worksheet Week 01 - Question 20)
+
+#include <stdio.h>
+int main()
+{
+    int a = 25;
+    float b = 25;
+    printf("%d", a);
+    return 0;
+}
---
+++ Exercise26.c
@@ -0,0 +1,8 @@
+// Variable in C (Supplementary Worksheet Week 01 - Question 21)
+
+#include <stdio.h>
+int main()
+{
+
+    return 0;
+}
