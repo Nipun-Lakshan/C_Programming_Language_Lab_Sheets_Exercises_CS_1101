edc54c32da4ad306f7007835244795f26c77cae2
Nipun Lakshan <nipunlakshan81@gmail.com>
2025-01-02 23:29:51 +0530

    Exercise 34

---
+++ Exercise35.c
@@ -0,0 +1,12 @@
+// Declaring Variables and Format Specifiers (Supplementary Worksheet Week 01 - Question 30)
+
+#include <stdio.h>
+
+int main()
+{
+    // Variable Declaration
+
+    // Print Results
+    
+    return 0;
+}
