ff035aaa28f313af5d4ffa4ec6e0ad0b402db44e
Nipun Lakshan <nipunlakshan81@gmail.com>
2025-01-02 23:28:02 +0530

    Exercise 33

--- Exercise33.c
+++ Exercise33.c
@@ -6,12 +6,11 @@ int main()
 {
     // Variable Declaration
     int a = 10;
-    int b = 15;
 
     // Print Results
-    printf("\n%d + %d = %d", a, b, (a + b));            // 25
-    printf("\n%d - %d = %d", a, b, (a - b));            // -5
-    printf("\n%d * %d = %d", a, b, (a * b));            // 150
-    printf("\n%d / %d = %.2f\n", a, b, (a / (float)b)); // 0.667
+    printf("\na = 10 and a++ : %d", (a++)); // 10
+    a = 10;
+    printf("\na = 10 and ++a : %d\n", (++a)); // 11
+
     return 0;
 }
---
+++ Exercise34.c
@@ -0,0 +1,16 @@
+// Declaring Variables and Format Specifiers (Supplementary Worksheet Week 01 - Question 29)
+
+#include <stdio.h>
+
+int main()
+{
+    // Variable Declaration
+    int a = 10;
+
+    // Print Results
+    printf("\na = 10 and a-- : %d", (a--)); // 10
+    a = 10;
+    printf("\na = 10 and --a : %d\n", (--a)); // 09
+
+    return 0;
+}
