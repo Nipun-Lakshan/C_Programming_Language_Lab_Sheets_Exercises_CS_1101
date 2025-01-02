37ded03f62187456779ec8f060c54c1c0d91de92
Nipun Lakshan <nipunlakshan81@gmail.com>
2025-01-02 22:57:31 +0530

    Exercise 31

--- Exercise31.c
+++ Exercise31.c
@@ -1,1 +1,18 @@
-// Start Here
+// Declaring Variables and Format Specifiers (Supplementary Worksheet Week 01 - Question 26)
+
+#include <stdio.h>
+
+int main()
+{
+    // Variable Declaration
+    short number = 10;
+    char letter = 'a';
+    float pi = 3.14;
+    double value = 1.2345678900;
+
+    printf("\n");
+    printf("Number   : %hd\n", number);
+    printf("Letter   : %c\n", letter);
+    printf("PI Value : %.2f\n", pi);
+    printf("Value    : %.10lf\n", value);
+}
