511223b09d5320c7633abf0daad9406aa813ce4f
Nipun Lakshan <nipunlakshan81@gmail.com>
2025-01-05 14:03:13 +0530

    Exercise 36

---
+++ Exercise37.c
@@ -0,0 +1,24 @@
+// Declaring Variables and Format Specifiers (Supplementary Worksheet Week 01 - Question 31 - Part 02)
+
+#include <stdio.h>
+
+int main()
+{
+    // Header String Formatting
+    printf("\n==========================");
+    printf("\nPerimeter of The Rectangle");
+    printf("\n==========================\n\n");
+
+    // Variable Declaration
+    double length = 7.342;
+    double width = 5.345;
+    double perimeter;
+
+    // Calculate the area
+    perimeter = 2 * (length + width);
+
+    // Print the Result
+    printf("The perimeter of the rectangle is %lf cm^2\n", perimeter);
+
+    return 0;
+}
