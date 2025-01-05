1bfdf26f304ab854d8bb50b5065e4ec31112cf37
Nipun Lakshan <nipunlakshan81@gmail.com>
2025-01-05 13:52:37 +0530

    Exercise 35

--- Exercise35.c
+++ Exercise35.c
@@ -4,9 +4,41 @@
 
 int main()
 {
-    // Variable Declaration
+    // Header String Formatting
+    printf("\n================================");
+    printf("\nSwap The Values of Two Variables");
+    printf("\n================================\n\n");
 
-    // Print Results
-    
+    // Variable Declaration
+    double a;
+    double b;
+    double temp; // Temporary variable to swap the values
+
+    // Request Data From User
+    printf("Enter a value for a : ");
+    scanf("%lf", &a);
+    printf("Enter a value for b : ");
+    scanf("%lf", &b);
+    printf("\n");
+
+    // Method 01 :- Swap the values of two variables
+    a = a + b;
+    b = a - b;
+    a = a - b;
+
+    // Print the result after Method 01.
+    printf("Current Value of a  : %.lf\n", a);
+    printf("Current Value of b  : %.lf\n", b);
+    printf("\n");
+
+    // Method 02 :- Swap the Values of Two Variables
+    temp = a;
+    a = b;
+    b = temp;
+
+    // Print the result after Method 02.
+    printf("Current Value of a  : %.lf\n", a);
+    printf("Current Value of b  : %.lf\n", b);
+
     return 0;
 }
