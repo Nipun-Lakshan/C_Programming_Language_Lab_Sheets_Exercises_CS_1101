69d1e8e31f1c095ddcb69591d30e7b7b5f75a058
Nipun Lakshan <nipunlakshan81@gmail.com>
2025-01-12 00:25:08 +0530

    Exercise 61

--- Exercise61.c
+++ Exercise61.c
@@ -2,7 +2,44 @@
 
 #include <stdio.h>
 
+// Function Declarations
+void decimalToBinary(int num);
+void decimalToHexadecimal(int num);
+
+// Main Function
 int main()
 {
+    // Variable Declaration
+    int num;
+
+    // Header String Formatting
+    printf("\n=========================================");
+    printf("\nConvert Decimal to Binary and Hexadecimal");
+    printf("\n=========================================\n\n");
+
+    // Input a Decimal Number
+    printf("Enter a Decimal Number : ");
+    scanf("%d", &num);
+
+    // Convert and Display the number in binary and hexadecimal
+    decimalToBinary(num);
+    decimalToHexadecimal(num);
+
     return 0;
+}
+
+void decimalToBinary(int n)
+{
+    printf("Binary Number\t       : ");
+    for (int i = 31; i >= 0; i--)
+    {
+        int bit = (n >> i) & 1;
+        printf("%d", bit);
+    }
+    printf("\n");
+}
+
+void decimalToHexadecimal(int n)
+{
+    printf("Hexadecimal Number     : %X\n", n);
 }
